a87ded9fab8b60d281a14051521c14280fc4c6d7
yashwanthcr7 <121919445+yashwanthcr7@users.noreply.github.com>
2023-05-11 23:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = ' my-mysql-container'
+app.config['MYSQL_HOST'] = 'my-mysql-container'
 app.config['MYSQL_USER'] = 'myuser'
 app.config['MYSQL_PASSWORD'] = 'mypassword'
 app.config['MYSQL_DB'] = 'mydb'
@@ -26,7 +26,9 @@ def home():
         )
     """)
     mysql.connection.commit()
+    cur.close()
     return render_template('home.html')
+
 @app.route('/add_patient', methods=['GET', 'POST'])
 def add_patient():
     if request.method == 'POST':
@@ -42,22 +44,23 @@ def add_patient():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO patients (first_name, last_name, dob, gender, diagnosis, covid, address) VALUES (%s, %s, %s, %s, %s, %s, %s)", (first_name, last_name, dob, gender, diagnosis, covid, address))
         mysql.connection.commit()
+        cur.close()
         return 'Patient added successfully'
     else:
         return render_template('add_patient.html')
 
 @app.route('/view_patients')
 def view_patients():
-       cur = mysql.connection.cursor()
+    cur = mysql.connection.cursor()
 
-       cur.execute("SELECT * FROM patients")
+    cur.execute("SELECT * FROM patients")
 
-       patients = cur.fetchall()
+    patients = cur.fetchall()
 
-       return render_template('view_patients.html', patients=patients)
-
+    cur.close()
+    return render_template('view_patients.html', patients=patients)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
-  
+
